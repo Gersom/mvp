c737d005e66ec1ac89d4ad551652eb962f46958b
gersom <gersomalaja@gmail.com>
2019-10-25 17:15:35 -0500

    Nuxt inicio

---
+++ src/components/Prueba.vue
@@ -0,0 +1,13 @@
+<template lang='pug'>
+    div()
+        p.c-prueba Esto es una prueba
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang='stylus' scoped>
+    .c-prueba
+        color #ff0000
+</style>
